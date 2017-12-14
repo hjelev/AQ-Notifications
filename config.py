98922f9fcdd55756889f4f5ba3ffa7235caf5508
hjelev <jeleff@gmail.com>
2017-12-14 14:48:47 +0200

    First commit

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ air_map_url = "http://maps.luftdaten.info/#13/"
 # configure smtp server for sending mail
 web.config.smtp_server = 'smtp.gmail.com'
 web.config.smtp_port = 587
-web.config.smtp_username = 'username@gmail.com'
-web.config.smtp_password = 'password'
+web.config.smtp_username = 'krissg@gmail.com'
+web.config.smtp_password = 'q1w2E#R$'
 web.config.smtp_starttls = True
 #Notification recipients
 email_list =	[
-					{"email":"username1@gmail.com", "station_id":"5690" },
-					{"email":"username2@gmail.com", "station_id":"5690" },
-					{"email":"username3@gmail.com", "station_id":"6771" },
-					{"email":"username4@gmail.com", "station_id":"6195" },
+					{"email":"jeleff@gmail.com", "station_id":"5690" },
+					{"email":"tanya.hristova@gmail.com", "station_id":"5690" },
+					{"email":"soteks@gmail.com", "station_id":"6771" },
+					{"email":"mita4e@gmail.com", "station_id":"6195" },
 				]
