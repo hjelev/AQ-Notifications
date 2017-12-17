7baae0387d1eb3d36c73dc13b32e56046e81ae46
hjelev <jeleff@gmail.com>
2017-12-17 12:27:51 +0200

    minor uptades

--- alerts.py
+++ alerts.py
@@ -3,10 +3,66 @@ from datetime import datetime
 api_url = "http://api.luftdaten.info/v1/sensor/"
 air_map_url = "http://maps.luftdaten.info/#13/"
 
+AQI = [
+		{"aqi": "0-50", "apl": "Good", 
+		"health_implications": "Air quality is considered satisfactory, and air pollution poses little or no risk", 
+		"cautionary_statement" : "None" 
+		},		
+		{"aqi": "51-100", "apl": "Moderate", 
+		"health_implications": "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.", 
+		"cautionary_statement" : "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion" 
+		},		
+		{"aqi": "101-150", "apl": "Unhealthy for Sensitive Groups", 
+		"health_implications": "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+		"cautionary_statement" : "Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion." 
+		},	
+		{"aqi": "151-200", "apl": "Unhealthy", 
+		"health_implications": "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects", 
+		"cautionary_statement" : "Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion" 
+		},		
+		{"aqi": "201-300", "apl": "Very Unhealthy", 
+		"health_implications": "Health warnings of emergency conditions. The entire population is more likely to be affected.", 
+		"cautionary_statement" : "Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion." 
+		},		
+		{"aqi": "300+", "apl": "Hazardous", 
+		"health_implications": "Health alert: everyone may experience more serious health effects", 
+		"cautionary_statement" : "Everyone should avoid all outdoor exertion" 
+		},
+	]
+
+
 def alert_message(p1, p2, station_id, location):
-	#compose email body from air polution alert
+	#get air pollutioin health message
+	if p2 < 50:
+		apl = AQI[0]['apl']
+		health_implications = AQI[0]['health_implications']
+		cautionary_statement = AQI[0]['cautionary_statement']
+	elif p2 >= 50 and p2 <= 100:
+		apl = AQI[1]['apl']
+		health_implications = AQI[1]['health_implications']
+		cautionary_statement = AQI[1]['cautionary_statement']
+	elif p2 >= 101 and p2 <= 150:
+		apl = AQI[2]['apl']
+		health_implications = AQI[2]['health_implications']
+		cautionary_statement = AQI[2]['cautionary_statement']
+	elif p2 >= 151 and p2 <= 200:
+		apl = AQI[3]['apl']
+		health_implications = AQI[3]['health_implications']
+		cautionary_statement = AQI[3]['cautionary_statement']		
+	elif p2 >= 201 and p2 <= 300:
+		apl = AQI[4]['apl']
+		health_implications = AQI[4]['health_implications']
+		cautionary_statement = AQI[4]['cautionary_statement']
+	elif p2 > 300:
+		apl = AQI[5]['apl']
+		health_implications = AQI[5]['health_implications']
+		cautionary_statement = AQI[5]['cautionary_statement']
+		
+	#compose email body for air polution alert
 	message = '''
-		<h1>High Air Pollution alert!</h1><br>
+		<h1>Air is {apl}!</h1><br>
+		Health Implications: {health_implications}<br><br>
+		Cautionary Statement: {cautionary_statement}<br><br>
 		Alert Date {today} <br>
 		<p>PM10 = {p1} µg/m³ <br> PM2.5 = {p2} µg/m³ </p>
 		<img src="https://api.luftdaten.info/grafana/render/dashboard-solo/db/single-sensor-view?orgId=1&panelId=2&width=300&height=200&tz=UTC%2B02%3A00&var-node={stationid}">
@@ -22,12 +78,15 @@ def alert_message(p1, p2, station_id, location):
 		<img src="https://maps.googleapis.com/maps/api/staticmap?center={location}&zoom=12&size=300x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C{location}&markers=color:red%7C&key=AIzaSyBmdDFqNyjLOUUwMzXmskur36QdbF4oPao">
 		</a>
 		<br>
-		'''.format(map_url = air_map_url + location.replace(",", "/"),
+		'''.format(	apl = apl,
+					health_implications = health_implications,
+					cautionary_statement = cautionary_statement,
+					map_url = air_map_url + location.replace(",", "/"),
 					today = datetime.today().strftime("%Y-%m-%d %H:%M"),
 					p1 = p1,
 					p2 = p2,
 					stationid = station_id,
-					location = location
+					location = location,
 					)
 	return message
 
@@ -56,7 +115,7 @@ def ok_message(ok_value, p1, p2, last_p1, last_p2,
 				<a href="{map_url}">
 				<img src="https://maps.googleapis.com/maps/api/staticmap?center={location}&zoom=12&size=300x200&maptype=roadmap&markers=color:blue%7Clabel:S%7C{location}&markers=color:red%7C&key=AIzaSyBmdDFqNyjLOUUwMzXmskur36QdbF4oPao">
 				</a>
-				<br>
+				<br><br>
 				<small>
 					Latest air pollution was on {lastpolutiondate}<br>
 					<br>
@@ -74,5 +133,6 @@ def ok_message(ok_value, p1, p2, last_p1, last_p2,
 					lastpolutiondate = last_alert_date,
 					last_p1 = last_p1,
 					last_p2 = last_p2,
-					location = location)	
+					location = location
+					)	
 	return message
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-import urllib.request, json, csv, os , atexit, logging
+import urllib.request, json, csv, os , logging
 from collections import deque
 from datetime import datetime
+from functools import reduce
 
 # import configuration variables
 import config as gl
 from alerts import *
 from send_email import *
-# todo alerts sev text http://aqicn.org/data-platform/register/
 
 # read air data from json api and return average values
 def get_air_data(station_id):
 	p1, p2 = [], []
 	with urllib.request.urlopen(api_url + station_id + "/") as url:
 		data = json.loads(url.read().decode())
+		#we need this as there are multiple sensor readings in the api response
 		for results in data:			
 			timestamp = results['timestamp']
 			if 'sensordatavalues' in results:
@@ -24,8 +25,10 @@ def get_air_data(station_id):
 						p1.append(float(readings['value']))	
 					elif readings['value_type'] == 'P2':
 						p2.append(float(readings['value']))						
-	print(p1, p2, location)
-	return int(sum(p1) / len(p1)), int(sum(p2) / len(p2)), timestamp, location
+	average_p1 = int(reduce(lambda x, y: x + y / float(len(p1)), p1, 0))
+	average_p2 = int(reduce(lambda x, y: x + y / float(len(p2)), p2, 0))
+
+	return average_p1, average_p2, timestamp, location
 
 # save the air data to a csv file
 def write_to_csv(p1, p2, timestamp, csv_path):
@@ -58,32 +61,45 @@ def check_dir(file_path):
 		os.stat(directory)
 	except:
 		os.mkdir(directory) 	
-	
+
+def enable_logging():	
+	log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+				gl.config.logs_file_name
+				)		
+	logging.basicConfig(filename=log_path,
+						format='%(asctime)s %(message)s',
+						datefmt='%m/%d/%Y %I:%M:%S %p',
+						level=logging.INFO
+						)
+						
 def main(user):
 	# get air pollution data
 	p1, p2, timestamp, location = get_air_data(user['station_id'])	
 	# Build the path to the csv file with previous air data
 	csv_path = os.path.join(os.path.dirname(os.path.realpath(__file__)) ,
 				gl.config.csv_data_folder ,
-				"air_data_{}-{}.csv".format(user['station_id'], user['email'].replace('@', '-'))
+				"air_data_{}-{}.csv".format(
+						user['station_id'],
+						user['email'].replace('@', '-')
+					)
 				)
-
 	# make sure the folder for the csv files exists
 	check_dir(csv_path)
-	# get last data from the last record in the csv file for the current user
+	# get previus air data from the csv file for the current user
 	last_p1, last_p2, last_alert_date = get_last_row(csv_path, p1, p2)
 
-	#p2 = 20 # current value of pm2.5 comment used for tests	
-	#last_p2 = 722 # previous value of pm2.5 comment used for tests
+	#p2 = 130 # current value of pm2.5 comment used for tests	
+	#last_p2 = 22 # previous value of pm2.5 comment used for tests
 	
-	# pollution is high
+	# check if there is air pollution
 	if p2 > gl.config.alert_value:	
-		# check if there was an alert today and if its value is > alert_value
+		# check if there was a pollution alert already send
 		# - no alert is send
 		if int(last_p2) > gl.config.alert_value and p2 > int(last_p2):
 			# record the new higher value
 			write_to_csv(p1, p2, timestamp, csv_path)
-		# check if last alert was positive and send polution alert		
+		# check if last alert was positive 
+		# - send polution alert		
 		elif int(last_p2) < gl.config.ok_value:
 			# sends email notifications
 			send_email(alert_message(p1, p2, user['station_id'], location),
@@ -94,41 +110,31 @@ def main(user):
 							+"PM10= {} PM2.5={}".format(p1, p2,))
 			# write new alert record to csv file
 			write_to_csv(p1, p2, timestamp, csv_path)
-	# check if polution is OK					
-	elif p2 < gl.config.ok_value:
-		# check if last alert was negative and send clear air alert 
-		if int(last_p2) > gl.config.ok_value:
-			# we need to send clear air alert
-			send_email(ok_message(gl.config.ok_value, p1, p2,
-										last_p1, last_p2,
-										user['station_id'],
-										last_alert_date,
-										location
-										),
-									"Clear Air Alert!",
-									user['email']
-									)
-			logging.info("- Clear Air Alert sent to {} ".format(user['email'])
-							+"PM10= {} PM2.5={}".format(p1, p2,))
-			# write new clear air record to the csv file
-			write_to_csv(p1, p2, timestamp, csv_path)
+	# check if air is clear			
+	# check if last alert was negative and send clear air alert 
+	# - send clear air alert	
+	elif p2 < gl.config.ok_value and int(last_p2) > gl.config.ok_value:
+		send_email(ok_message(gl.config.ok_value, p1, p2,
+									last_p1, last_p2,
+									user['station_id'],
+									last_alert_date,
+									location
+									),
+								"Clear Air Alert!",
+								user['email']
+								)
+		logging.info("- Clear Air Alert sent to {} ".format(user['email'])
+						+"PM10= {} PM2.5={}".format(p1, p2,))
+		# write new clear air record to the csv file
+		write_to_csv(p1, p2, timestamp, csv_path)
 	return
-
-if __name__ == '__main__':
-
-	log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-			gl.config.logs_file_name
-			)		
-	logging.basicConfig(filename=log_path,
-						format='%(asctime)s %(message)s',
-						datefmt='%m/%d/%Y %I:%M:%S %p',
-						level=logging.INFO
-						)
 						
+if __name__ == '__main__':
+	enable_logging()
 	for user in gl.config.email_list:
 		#main(user)
 		try:
 			main(user)
-		except:
-			print("Problem with user: ", user)
-			logging.error("Problem with user: " + user['email'])
+		except Exception as e:
+			print("Problem with user: ", user['email'],e)
+			logging.error("Problem with user: {} {}".format(user['email'], e))
